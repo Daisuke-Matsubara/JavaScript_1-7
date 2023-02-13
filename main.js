830f3b51168cdb7b1dfb28776d53b8b15aa05c9a
Daisuke-Matsubara <110824363+Daisuke-Matsubara@users.noreply.github.com>
2023-02-13 22:23:29 +0900

    JavaScript初級第1章クラスとインスタンス 課題

--- main.js
+++ main.js
@@ -1,21 +1,18 @@
 //Humanクラスを作成
-class Human {
-    constructor(name,height,weight,gender,age){
-        this.name = name;
-        this.height = height;
-        this.weight = weight;
-        this.gender = gender;
-        this.age = age;
+class Taiyaki {
+    constructor(nakami){
+        this.nakami = nakami;
     }
 
-    //歩くメソッド作成
-    walk(){
-        console.log(`身長${this.height}cm、体重${this.weight}kg、${this.name}という${this.gender}が歩きました`);
+    Nakami(){
+        console.log(`中身は${this.nakami}です`);
     }
 }
-//Humanクラスをインスタンス化
-let yamada = new Human('yamada',170,60,'男',30);
-let suzuki = new Human('suzuki',180,70,'男',35);
-console.log(yamada.name,yamada.height,yamada.gender);
-yamada.walk();
-console.log(suzuki.age);
+
+let anko = new Taiyaki('あんこ');
+let cream = new Taiyaki('クリーム');
+let cheese = new Taiyaki('チーズ');
+
+anko.Nakami();
+cream.Nakami();
+cheese.Nakami();
