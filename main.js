62b214345d9e9a6be989365c445a872449f72e89
Daisuke-Matsubara <110824363+Daisuke-Matsubara@users.noreply.github.com>
2023-02-13 22:09:14 +0900

    R5.2.13

--- main.js
+++ main.js
@@ -7,9 +7,15 @@ class Human {
         this.gender = gender;
         this.age = age;
     }
+
+    //歩くメソッド作成
+    walk(){
+        console.log(`身長${this.height}cm、体重${this.weight}kg、${this.name}という${this.gender}が歩きました`);
+    }
 }
 //Humanクラスをインスタンス化
 let yamada = new Human('yamada',170,60,'男',30);
 let suzuki = new Human('suzuki',180,70,'男',35);
 console.log(yamada.name,yamada.height,yamada.gender);
+yamada.walk();
 console.log(suzuki.age);
